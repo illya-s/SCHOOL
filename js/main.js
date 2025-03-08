122ce6ba368b1ba59be2e95128bbed5ef24d1271
Ilya <107246761+illya-s@users.noreply.github.com>
2025-03-08 12:06:45 +0200

    update

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const elements = document.querySelectorAll("main > div");
-    const count = Math.floor(elements.length / 5); // Количество элементов, которые нужно изменить
+// document.addEventListener("DOMContentLoaded", () => {
+//     const elements = document.querySelectorAll("main > div");
+//     const count = Math.floor(elements.length / 6);
 
-    let selectedIndexes = new Set();
-    while (selectedIndexes.size < count) {
-        let randomIndex = Math.floor(Math.random() * elements.length);
-        selectedIndexes.add(randomIndex);
-    }
+//     let selectedIndexes = new Set();
+//     while (selectedIndexes.size < count) {
+//         let randomIndex = Math.floor(Math.random() * elements.length);
+//         selectedIndexes.add(randomIndex);
+//     }
 
-    selectedIndexes.forEach(index => {
-        elements[index].style.gridColumn = "span 2";
-    });
-});
+//     selectedIndexes.forEach(index => {
+//         elements[index].style.gridColumn = "span 2";
+//     });
+// });
