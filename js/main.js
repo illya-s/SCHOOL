f07cfeac2e28117a63c1e9d006aff572454860db
Ilya <107246761+illya-s@users.noreply.github.com>
2025-03-17 11:43:54 +0200

    update

--- js/main.js
+++ js/main.js
@@ -1,3 +1,43 @@
+$(document).ready(function () {
+	const dialog = $('.dialog');
+	const dialogImage = $('.dialog-image');
+	const downloadImage = $("#downloadImage");
+
+	function Hash() {
+		return location.hash.substring(1);
+	}
+	if (Hash() != '') {
+		open_dial()
+	}
+
+
+	dialog.on('click', function (e) {
+		if ($(e.target).is('.dialog')) {
+			window.location.hash = ""
+		}
+	})
+
+	$(window).on('hashchange', function (e) {
+		if (Hash() != '') {
+			open_dial()
+		} else {
+			dialog.removeClass('active')
+		}
+	})
+
+	function open_dial() {
+		var element = $(`#id${Hash()}`)
+		var bgImage = $(element).css("background-image");
+		var url = bgImage.replace(/url\(["']?(.*?)["']?\)/, '$1');
+
+		dialogImage.attr('src', url)
+		downloadImage.attr('href', url)
+
+		dialog.addClass('active')
+	}
+});
+
+
 // document.addEventListener("DOMContentLoaded", () => {
 //     const elements = document.querySelectorAll("main > div");
 //     const count = Math.floor(elements.length / 6);
